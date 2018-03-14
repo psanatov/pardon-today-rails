ed837769f2a63687fd1b14c76ed0b7b2b8827ff9
Pavel Sanatov <psanatov@gmail.com>
2018-03-14 10:50:09 +0100

    Added like/unlike functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  acts_as_voter
+
+  def likes?(post)
+    post.likes.where(user_id: id).any?
+  end
 end
